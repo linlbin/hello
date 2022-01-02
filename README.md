0214c8c9f3aeea4b251aedbc1bdea6d9c04cb153
leon lin <leonlinlibin@hotmail.com>
2022-01-02 22:12:31 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+﻿Hi I am Leon
+This is my first readme file
+thankyou
+
+我是我，
+你不是你
+
---
+++ hello.js
@@ -0,0 +1,9 @@
+﻿var http = require('http');
+
+http.createServer(function(req, res){
+	res.writeHead(200,{'Content-Type': 'text/plain;charset=UTF-8'});
+	
+	res.end('Hello ZWY, Fuck you草有');
+}).listen(8888);
+
+console.log('Server running at http://127.0.0.1:8888');
